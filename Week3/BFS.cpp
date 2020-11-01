fc606bd89b3f18582f2eda04748743ca268217e3
Vira Maksymets <55591954+ViraMaximets@users.noreply.github.com>
2020-11-01 15:55:02 +0300

    Add files via upload

---
+++ Week3/BFS.cpp
@@ -0,0 +1,73 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+using std::vector;
+using std::queue;
+
+vector <int> dist;
+queue <int> que;
+int inf, start, ende;
+
+int distance(int currDist, vector<vector<int> >& adj, int start, int ende) {
+
+    if (start == ende)
+        return 0;
+
+    dist[start] = 0;
+    que.push(start);
+
+    while (!que.empty())
+    {
+        int u = que.front();
+        que.pop();
+
+
+        for (int i = 0; i < adj[u].size(); i++)
+        {
+            if (dist[adj[u][i]] == inf)
+            {
+                que.push(adj[u][i]);
+                dist[adj[u][i]] = dist[u] + 1;
+
+            }
+        }
+
+
+    }
+
+    if (dist[ende] != inf)
+    {
+        return dist[ende];
+    }
+
+    return -1;
+}
+
+int main() {
+    int n, m;
+    std::cin >> n >> m;
+
+    inf = n + n;
+    dist.resize(n, inf);
+
+
+    vector<vector<int> > adj(n, vector<int>());
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        std::cin >> x >> y;
+        adj[x - 1].push_back(y - 1);
+        adj[y - 1].push_back(x - 1);
+    }
+
+
+    cin >> start >> ende;
+    start--, ende--;
+
+    int currDist = 0;
+
+    cout << distance(currDist, adj, start, ende);
+
+    return 0;
+}
+
---
+++ Week3/Bipartite.cpp
@@ -0,0 +1,75 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector <int> color;
+queue <int> que;
+int cases;
+
+int bipartite(vector<vector<int> >& adj, int start, int pred) {
+
+    //white = 1, black = 2
+    while (!que.empty())
+    {
+        int u = que.front();
+        que.pop();
+
+        for (int i = 0; i < adj[u].size(); i++)
+        {
+            if (color[u] == 1)
+                cases = 1;
+            else
+                cases = 2;
+            
+            
+            if (color[adj[u][i]] == 0)
+            {
+                que.push(adj[u][i]);
+
+                if (cases == 1)
+                {
+                    color[adj[u][i]] = 2;
+                }
+                else if (cases == 2)
+                {
+                    color[adj[u][i]] = 1;
+                }
+            }
+
+            else if (adj[u][i] != pred)
+            {
+                if ((cases == 1)&&(color[adj[u][i]] == 1))
+                {
+                    return 0;
+                }
+                if ((cases == 2) && (color[adj[u][i]] == 2))
+                {
+                    return 0;
+                }
+            }
+        }
+    }
+
+    return 1;
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    color.assign(n, 0);
+
+    vector<vector<int> > adj(n, vector<int>());
+    for (int i = 0; i < m; i++) {
+        int x, y;
+        cin >> x >> y;
+        adj[x - 1].push_back(y - 1);
+        adj[y - 1].push_back(x - 1);
+    }
+
+    color[0] = 1;
+    que.push(0);
+
+    cout << bipartite(adj, 0, -1);
+}
