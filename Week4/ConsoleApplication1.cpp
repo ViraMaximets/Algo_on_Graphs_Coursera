671f80885e62af7fa0f32d388d711b10d74cd24d
Vira Maksymets <55591954+ViraMaximets@users.noreply.github.com>
2020-11-01 15:58:50 +0300

    Add files via upload

---
+++ Week4/ConsoleApplication1.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+long int inf = 99999;
+vector <int> dist;
+
+int negative_cycle(vector<vector<int> >& adj, vector<vector<int> >& cost) {
+
+    for (int q = 0; q < n; q++)
+    {
+        for (int u = 0; u < n; u++)
+        {
+            for (int k = 0; k < adj[u].size(); k++)
+            {
+                if (dist[adj[u][k]] > dist[u] + cost[u][k])
+                {
+                    dist[adj[u][k]] = dist[u] + cost[u][k];
+                    if (q == n - 1)
+                        return 1;
+                }
+            }
+        }
+    }
+    return 0;
+}
+
+int main() {
+
+    cin >> n >> m;
+
+    vector<vector<int> > adj(n, vector<int>());
+    vector<vector<int> > cost(n, vector<int>());
+
+    dist.assign(n, inf);
+    dist[0] = 0;
+
+    for (int i = 0; i < m; i++) 
+    {
+        int x, y, w;
+        cin >> x >> y >> w;
+
+        adj[x - 1].push_back(y - 1);
+        cost[x - 1].push_back(w);
+    }
+
+    cout << negative_cycle(adj, cost);
+    return 0;
+}
---
+++ Week4/Fly_cost.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+#include <vector>
+#include <queue>
+#include <cmath>
+
+using namespace std;
+
+vector <int> dist;
+queue <int> que;
+long double inf;
+
+int distanc(vector<vector<int> >& adj, vector<vector<int> >& cost, int start, int ende) {
+
+    while (!que.empty())
+    {
+        int u = que.front();
+        que.pop();
+
+        for (int i = 0; i < adj[u].size(); i++)
+        {
+            if (dist[adj[u][i]] > (dist[u] + cost[u][i]))
+            {
+                que.push(adj[u][i]);
+                dist[adj[u][i]] = dist[u] + cost[u][i];
+            }
+        }
+    }
+
+    return 0; 
+}
+
+int main() {
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int> > adj(n, vector<int>());
+    vector<vector<int> > cost(n, vector<int>());
+
+    for (int i = 0; i < m; i++) 
+    {
+        int x, y, w;
+        cin >> x >> y >> w;
+        adj[x - 1].push_back(y - 1);
+        cost[x - 1].push_back(w);
+    }
+
+    inf = pow(10, 9)+m;
+    dist.assign(n, inf);
+
+    int start, ende;
+    cin >> start >> ende;
+    start--, ende--;
+
+    dist[start] = 0;
+    que.push(start);
+
+    if (start == ende)
+    {
+        cout << dist[start];
+        return 0;
+    }
+
+    distanc(adj, cost, start, ende);
+
+    if (dist[ende] != inf)
+    {
+        cout << dist[ende];
+    }
+
+    else
+        cout << -1;
+}
